96796f62e748c4bc027ab2beb34c99f67f55c359
Ed <edwardmartin.inbox@gmail.com>
2021-08-09 23:04:12 +0100

    single address API working

---
+++ API.rb
@@ -0,0 +1,50 @@
+#! /usr/bin/env ruby
+########################################### 
+###########################################
+
+# Run Me From "play" Directory ### ./play.rb
+# Run Me From Anywhere         ###  /Users/ed/Desktop/Makers/play/play.rb
+
+###########################################
+###########################################
+# Code:
+# Where I got this guide from
+# https://ledumjg.medium.com/detailed-guide-on-etherscan-ethereum-developers-apis-5e12934f728a
+# 
+# API Request
+# Module = account
+# Action = balance
+# Address = the address you want to infer information from
+# Tag = latest, so that you get the latest balance
+# Key = Your APIKeyToken
+
+require 'rest-client'
+require 'json'
+require 'dotenv'
+
+Dotenv.load
+
+address = "0xa95aea385130718be87b380b419eeac8da40de55"
+# [
+#   "0xa95aea385130718be87b380b419eeac8da40de55", 
+#   "0x0dd80fcd70370650e79db794723e31848a593459", 
+#   "0x4e7e1c73c116649c1c684acb6ec98bac4fbb4ef6", 
+#   "0xc55ba66cab0298b3a67e1d0bf6a1613907941b09", 
+#   "0xddee597e2404149c5e9aea899dfdf7c15874245e"
+# ]
+key = ENV["KEY"]
+url = "https://api.etherscan.io/api?module=account&action=txlist&address=#{address}&startblock=0&endblock=99999999&sort=asc&apikey=#{key}"
+web = Hash.new
+
+ 
+# address.map { |target_address| 
+  
+# }
+
+response = RestClient.get url
+jsonResponse = JSON.parse(response)
+
+jsonResponse["result"].each { | transaction |
+  puts transaction["from"]
+  puts transaction["to"]
+}
