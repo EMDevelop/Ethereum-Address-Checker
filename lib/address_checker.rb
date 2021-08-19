a3536820548372559c12bb2fbdae0bc743f8c0f0
EMDevelop <edwardmartin.inbox@gmail.com>
2021-08-19 20:54:21 +0100

    added functionality to only allow  addition of 2 addresses

--- lib/address_checker.rb
+++ lib/address_checker.rb
@@ -6,29 +6,45 @@ class AddressChecker
 
   attr_reader :origin_addresses
 
-  def handle_manual_input 
-      puts "Add at least 2 Addresses, hit enter after each address, type 'quit' when done"
-      while true do
-        address = gets.chomp
-        break if address == "quit"
-        check_ethereum_address_validity(address) ? @origin_addresses << address : (puts "Error: Invalid Ethereum Address")
-      end
-  end
-
-
-  def input_origin_addresses_manually?(y_n)  
-
+  def input_origin_addresses_manually(y_n)  
     if y_n == "y"
-      handle_manual_input
+      handle_manual__address_input
     elsif y_n == "n"
       @origin_addresses = ["0x72140C1886f8F2Dd932DCe06795901F8FB6378a7","0x0613Cd2076bd432C7A60a1b926b11B17BaAaFE11"]
     else 
       print "Please only enter 'y' or 'n' into 'input_origin_addresses_manually'"
     end
-      
   end
 
-  def check_ethereum_address_validity(address)
+  def handle_manual__address_input 
+      puts "Add at least 2 Addresses, hit enter after each address, type 'quit' when done"
+      input_count = 0
+      while true do
+        address = input_address
+        break if handle_address_input_outcome(input_count, address) == 'quit'
+        input_count += 1
+      end
+  end
+
+  def handle_address_input_outcome(count, input)
+    if count >= 2 && input == "quit"
+      return 'quit'
+    elsif count < 2 && input == "quit"
+      puts "Error: Please add at least 2 addresses"
+    else
+      check_address_validity(input) ? handle_address_storing(input) : (puts "Error: Invalid Ethereum Address")
+    end
+  end
+
+  def handle_address_storing(input)
+    @origin_addresses << input
+  end
+
+  def input_address
+     gets.chomp
+  end
+
+  def check_address_validity(address)
     address.downcase!
     valid_address = true
     valid_address = false if address.length != 42 || address[0..1] != "0x"
@@ -36,15 +52,8 @@ class AddressChecker
   end
 
   def main_menu
-    1
+    2
   end
   
 
 end
-
-
-
-address_investigation = AddressChecker.new
-
-
-
--- spec/address_checker_spec.rb
+++ spec/address_checker_spec.rb
@@ -1,53 +1,69 @@
 require 'address_checker'
 
+#irb -r './lib/address_checker.rb'
+#a = AddressChecker.new
+
+
 describe AddressChecker do   
 
-  subject = AddressChecker.new
 
-  context 'Main Menu' do
-    it 'checks main menu exists' do
-      expect(subject.main_menu).to eq 1
+  context 'Fetching API Data' do
+  
+  end
+
+  context 'User Inputting Address: Incorrect Details Provided' do
+
+    before(:each) do
+      allow(subject).to receive(:gets).and_return("0xa95aea385130718be87b380b419eeac8da40de55", "quit" )
+    end
+
+    it 'Throws error if there is < 2 addresses provided' do
+      
+      expect do
+        subject.handle_manual__address_input
+      end.to output(include("Add at least 2 Addresses, hit enter after each address, type 'quit' when done", "Error: Please add at least 2 addresses")).to_stdout 
+      output().to_stdout 
     end
   end
 
-  context 'Check Input Data.' do
+  context 'User Inputting Address: Correct Details Provided' do
 
-  
-    it 'if input is not y or n for manual address input, print error' do
+    before(:each) do
+      allow(subject).to receive(:gets).and_return("0x72140C1886f8F2Dd932DCe06795901F8FB6378a7", "0x72140C1886f8F2Dd932DCe06795901F8FB6378a7", "quit" )
+    end
+
+    it 'After input, there should be at least 2 addresses stored to operate tasks on' do
+      subject.input_origin_addresses_manually("y")
+      expect(subject.origin_addresses.length).to be > 1
+    end
+
+
+  end
+
+  context 'User Inputting Address: General Checks' do
+ 
+    it 'if input is not y or n for manual address input, print error' do      
       expect do
-        subject.input_origin_addresses_manually?("h")
+        subject.input_origin_addresses_manually("h")
       end.to output("Please only enter 'y' or 'n' into 'input_origin_addresses_manually'").to_stdout 
     end
 
     it 'If user wants to use default addresses, make sure origin_addresses equals my default 2 addresses' do
-      subject.input_origin_addresses_manually?("n")
+      subject.input_origin_addresses_manually("n")
       expect(subject.origin_addresses).to eq ["0x72140C1886f8F2Dd932DCe06795901F8FB6378a7","0x0613Cd2076bd432C7A60a1b926b11B17BaAaFE11" ]
     end
 
-    it 'If user wants to add their own addresses, check that they add more than 1 address' do
-      subject.input_origin_addresses_manually?("y")
-      # let(:user_input) { ["rock\n", "rock\n"] }
-      # allow_any_instance_of(Object).to receive(:gets).and_return('0xa95aea385130718be87b380b419eeac8da40de55', '0xa95aea385130718be87b380b419eeac8da40de55', 'quit')
-      # expect(subject).to receive(:puts).with("Add at least 2 Addresses, hit enter after each address, type 'quit' when done")
-      # allow(subject).to receive(:gets).and_return('0xa95aea385130718be87b380b419eeac8da40de55', '0xa95aea385130718be87b380b419eeac8da40de55', 'quit')
-      # subject.stub(gets: 'user input')
-      # expect(STDOUT).to receive(:puts).with("Add at least 2 Addresses, hit enter after each address, type 'quit' when done")
-      # allow(STDIN).to receive(:gets).and_return('0xa95aea385130718be87b380b419eeac8da40de55', '0xa95aea385130718be87b380b419eeac8da40de55', 'quit')
-      # expect($stdout).to receive(:puts).with("Add at least 2 Addresses, hit enter after each address, type 'quit' when done")
-      # allow($stdin).to receive(:gets).and_return('0xa95aea385130718be87b380b419eeac8da40de55', '0xa95aea385130718be87b380b419eeac8da40de55', 'quit')
-      expect(subject.origin_addresses.length).to be > 1
-    end
 
     it 'checks if the input address is correct length' do
-      expect(subject.check_ethereum_address_validity("0x72140C1886f8F2D06795901FB6378a7")).to eq false
+      expect(subject.check_address_validity("0x72140C1886f8F2D06795901FB6378a7")).to eq false
     end
 
     it 'checks if the input address written in Hex (starts with) with 0x' do
-      expect(subject.check_ethereum_address_validity("px72140C1886f8F2Dd932DCe06795901FB6378a7")).to eq false
+      expect(subject.check_address_validity("px72140C1886f8F2Dd932DCe06795901FB6378a7")).to eq false
     end
 
     it 'checks if a valid address will address will be accepted' do
-      expect(subject.check_ethereum_address_validity("0xa95aea385130718be87b380b419eeac8da40de55")).to eq true
+      expect(subject.check_address_validity("0xa95aea385130718be87b380b419eeac8da40de55")).to eq true
     end
 
   end 
